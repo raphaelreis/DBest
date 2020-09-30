c874bdad234e2f3a8625221477d494f265c1b227
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-09-30 11:44:54 +0200

    keep avg and var queries.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,7 @@ import org.apache.spark.sql.types.StructType
 import java.nio.file.{Paths, Files}
 import org.apache.log4j.{Level, Logger}
 import Sampler._
+import org.apache.spark.sql.functions._
 
 
 object DBest {
@@ -17,7 +18,8 @@ object DBest {
   
     // val root = "file:///scratch/ml_aqp/"
     // val fileName = "data/sf10/store_sales.dat"
-
+    val root = ""
+    val fileName = "data/store_sales_sample.dat"
     if (Files.exists(Paths.get(fileName))) {
       logger.info(fileName + " exists")
 
@@ -27,6 +29,7 @@ object DBest {
         .option("inferSchema", "true")
         .option("mode", "DROPMALFORMED")
         .load(root + fileName).drop("_c23")
+      df.createOrReplaceTempView("store_sales")
 
       //sampling
       val fraction = 0.2
@@ -36,13 +39,16 @@ object DBest {
 
       //Queries
       //1)
-      val res1 = sampled_df.select("_c13")
-      //2)
-      val res2 = sampled_df.where("_c10 BETWEEN 50 AND 100").count()
-      //3)
-      val res3 = sampled_df.select("_c0", "_c20").groupBy("_c0").sum("_c20")
+      val q1 = "SELECT AVG(_c13), VARIANCE(_c13) FROM store_sales"
+      val res1 = spark.sqlContext.sql(q1)
+      spark.time(res1.show())
+      
+      
 
-      //Metrics
+      // logger.info(s"t10: $t10 | t11: $t11 | t20: $t20 | t21: $t21 | t30: $t30 | t31: $t31")
+      // logger.info(s"t10: $t10 | t11: $t11")
+      // logger.info(s"res1: $res1 | res1Sample: $res1Sample")
+
     } else {
       logger.info(fileName + " does not exist !")
     }
