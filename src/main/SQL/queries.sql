7ea1436c8e2a0feed5841648cc55934af9a92a28
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-10-14 23:50:31 +0200

    update queries of interest

--- src/main/SQL/queries.sql
+++ src/main/SQL/queries.sql
@@ -1,13 +1,13 @@
 
 -- Store_sales.data table queries -- 
--- Average price of sales
-SELECT AVG("_c13") FROM store_sales;
-
--- Count the number of sales for ticket_number 1
+-- 1) Count the number of sales for ticket_number 1
 SELECT COUNT(*) FROM store_sales
-WHERE "_c10" BETWEEN 50 AND 100;
+WHERE "_c12" BETWEEN 50 AND 100;
 
--- Net paid sum of all transactions grouped by day
-SELECT "_c0", SUM("_c20")
-FROM store_sales
-GROUP BY "_c0"
+-- 2) Average price of sales
+SELECT AVG("_c20") FROM store_sales
+WHERE "_c13" BETWEEN 5 AND 70;
+
+-- 3) Average price of sales
+SELECT SUM("_c20") FROM store_sales
+WHERE "_c13" BETWEEN 5 AND 70;
