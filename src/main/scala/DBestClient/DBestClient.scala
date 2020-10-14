c1e134da2cb9fccd3027b04fbca893817ea6b3fe
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-10-14 23:47:49 +0200

    add AQP for avg and sum

--- src/main/scala/DBestClient/DBestClient.scala
+++ src/main/scala/DBestClient/DBestClient.scala
@@ -6,6 +6,7 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.mllib.stat.KernelDensity
 import Ml._
 import QueryEngine._
+import org.apache.spark.ml.regression.LinearRegressionModel
 
 
 class DBestClient(dataset: String) {
@@ -44,10 +45,9 @@ class DBestClient(dataset: String) {
 
     def simpleQuery1() {
         /** Run simple count query with filtering */
-        val q2 = "SELECT COUNT(*) FROM store_sales WHERE _c12 BETWEEN 50 AND 100"
-        val res2 = spark.sqlContext.sql(q2)
-        println("Hello People !")
-        spark.time(res2.show())
+        val q1 = "SELECT COUNT(*) FROM store_sales WHERE _c12 BETWEEN 50 AND 100"
+        val res1 = spark.sqlContext.sql(q1)
+        spark.time(res1.show())
     }
 
     def simpleQuery1WithModel() {
@@ -55,8 +55,9 @@ class DBestClient(dataset: String) {
           * Same as simpleQuery1 but with AQP
           */
         val d = new SparkKernelDensity(3.0)
-        val kde = d.fit(df, "_c12")
-        val qe = new QueryEngine(kde, df.count().toInt)
+        val kde = d.fit(df, Array("_c12"))
+        val lr = new LinearRegressor
+        val qe = new QueryEngine(spark, kde, lr.model, df.count().toInt)
 
         val (count, elipseTime) = qe.approxCount(50, 100, 0.01)
 
@@ -66,10 +67,50 @@ class DBestClient(dataset: String) {
     }
 
     def simpleQuery2() {
-        /** Run simple average query and variance*/
-        val q1 = "SELECT AVG(_c13), VARIANCE(_c13) FROM store_sales"
-        val res1 = spark.sqlContext.sql(q1)
-        spark.time(res1.show())
+        val q2 = "SELECT AVG(_c20) FROM store_sales WHERE _c13 BETWEEN 5 AND 70"
+        val res2 = spark.sqlContext.sql(q2)
+        spark.time(res2.show())
     }
 
+    def simpleQuery2WithModel() {
+
+        val x = Array("_c13")
+        val y = "_c20"
+
+        val d = new SparkKernelDensity(3.0)
+        val kde = d.fit(df, x)
+        val lr = new LinearRegressor
+        val lrm = lr.fit(df, x, y)
+
+        val qe = new QueryEngine(spark, kde, lrm, df.count().toInt)
+        val (avg, elipseTime) = qe.approxAvg(df, x, 5, 70, 0.01)
+
+        println(s"AVG value with model: $avg")
+        println(s"Time to compute count: $elipseTime")
+
+    }
+
+    def simpleQuery3() {
+        val q3 = "SELECT SUM(_c20) FROM store_sales WHERE _c13 BETWEEN 5 AND 70"
+        val res3 = spark.sqlContext.sql(q3)
+        spark.time(res3.show())
+    }
+
+    def simpleQuery3WithModel() {
+
+        val x = Array("_c13")
+        val y = "_c20"
+
+        val d = new SparkKernelDensity(3.0)
+        val kde = d.fit(df, x)
+        val lr = new LinearRegressor
+        val lrm = lr.fit(df, x, y)
+
+        val qe = new QueryEngine(spark, kde, lrm, df.count().toInt)
+        val (sum, elipseTime) = qe.approxSum(df, x, 5, 70, 0.01)
+
+        println(s"AVG value with model: $avg")
+        println(s"Time to compute count: $elipseTime")
+
+    }
 }
