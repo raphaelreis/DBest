3ed08ae958c6162034164a35eb100e100c1f4b33
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2020-11-10 20:44:00 +0100

    add hasColumn test function

--- src/main/scala/Tools/Tools.scala
+++ src/main/scala/Tools/Tools.scala
@@ -1,10 +1,18 @@
 package Tools
 
 import Ml.DBestModel
+import scala.util.Try
+import org.apache.spark.sql.DataFrame
 
 object makeFileName extends ((DBestModel, Array[String], String) => String) {
     def apply(model: DBestModel, x: Array[String], y: String): String = {
         val root = "/Users/Raphael/CS/github.com/DBest/src/main/resources/models/"
         root + model.name + "/" + x.mkString("_") + y
+    }
+}
+
+object hasColumn extends ((DataFrame, String) => Boolean) {
+    def apply(df: DataFrame, col: String): Boolean = {
+        Try(df(col)).isSuccess
     }
 }
