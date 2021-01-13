941a24685f48af0138ad1bf2e9e2e8bee065741e
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2021-01-13 22:20:22 +0100

    implement stratified sampler

--- src/main/scala/Sampler/Sampler.scala
+++ src/main/scala/Sampler/Sampler.scala
@@ -1,14 +1,24 @@
 package sampler
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.SparkSession
 
 object Sampler {
     //Uniform sampling
     def uniformSampling(df: DataFrame, fraction: Double): DataFrame = df.sample(fraction) 
 
     //Stratified sampling
-    // def stratSampling[T](df: org.apache.spark.sql.DataFrame, col: org.apache.spark.sql.Column, frac: java.util.Map[T,java.lang.Double], seed: Long)
-        // = df.stat.sampleBy(col, frac, seed)
-
+    def stratifiedSampling(spark: SparkSession, df: DataFrame, groupByColumn: String, features: Array[String],
+        label: String,  fraction: Map[Int, Double]) = {
+            import spark.implicits._
+            df.select(groupByColumn, features(0), label)
+                .rdd
+                .map(r=> (r.getInt(0), (r.getDouble(1), r.getDouble(2))))
+                .sampleByKey(false, fraction)
+                .map{
+                    case (g, (f, l)) => (g, f, l)
+                }
+                .toDF(groupByColumn, features(0), label)           
+    }
     //Reservoir sampling
 }
