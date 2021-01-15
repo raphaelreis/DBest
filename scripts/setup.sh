30197e55637eb0ae046be878c247fad17130feba
Raphaël Reis Nunes <raphael.reisnunes@epfl.ch>
2021-01-15 16:55:13 +0100

    adding materials

---
+++ scripts/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+mkdir models
+cd models
+mkdir regressions
+mkdir densities
+mkdir overhead_models
+cd overhead_models
+mkdir regressions
+mkdir densities
+cd ../../
+mkdir results
+cd results
+mkdir local_results
+cd ..
